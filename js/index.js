aef319b3ce4184bb1ad4af278f77ebf8cd56c5d4
Morshaedul <morshaedulanirbanit@gmail.com>
2022-03-07 11:04:30 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$("input").focus(function(){
+    $(this).css("background","#97F491")
+});
+$("input").blur(function(){
+    $(this).css("background","gray")
+})
+
+
+
+
+
+
+
+$("input").mouseenter(function(){
+    $(this).css("background","#3095D9")
+})
+$("input").mouseleave(function(){
+    $(this).css("background","")
+})
